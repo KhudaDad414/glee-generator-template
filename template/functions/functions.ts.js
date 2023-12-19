0a8ed046b6542997bd9ddbdef80121798ed0f408
Khuda Dad Nomani <khudadad414@gmail.com>
2023-12-19 18:24:30 +0000

    generate function

--- template/functions/functions.ts.js
+++ template/functions/functions.ts.js
@@ -1,6 +1,12 @@
-import { File } from '@asyncapi/generator-react-sdk'
+import { File, Text } from '@asyncapi/generator-react-sdk'
 
 
-export default function ({ asyncapi }) {
-  return asyncapi.operations().filterByReceive().map(operation => (<File name={operation.id() + '.ts'}>//Code Here</File>))
+export default async function ({ asyncapi }) {
+  return asyncapi.operations().filterByReceive().map(operation => (<File name={operation.id() + '.ts'}>
+  <Text indent={0} newLines={2}>{'import { GleeFunction } from "@asyncapi/glee"'}</Text>
+  <Text indent={0} newLines={1}>{`const ${operation.id()}: GleeFunction = async ({ payload }) => {`}</Text>
+  <Text indent={1} newLines={1}>{'// Your business logic goes here...'}</Text>
+  <Text indent={0} newLines={1}>{'}'}</Text>
+  <Text indent={0} newLines={1}>{`export default ${operation.id()}`}</Text>
+  </File>))
 }

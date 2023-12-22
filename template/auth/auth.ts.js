1a4f28fc80755cc2505844abc975c17912b335a3
Khuda Dad Nomani <khudadad414@gmail.com>
2023-12-22 14:10:37 +0000

    add auth function generation

--- template/auth/auth.ts.js
+++ template/auth/auth.ts.js
@@ -1,6 +1,24 @@
-import { File } from '@asyncapi/generator-react-sdk'
+import { File, Text } from '@asyncapi/generator-react-sdk'
 
 
 export default function ({ asyncapi }) {
-  return asyncapi.servers().all().filter(server => server.security().length > 0).map(server => <File name={server.id() + '.ts'}>// Code Here</File>)
+  const serversWithAuth = asyncapi.servers().all().filter(server => server.security().length > 0)
+  return serversWithAuth.map(server =>{
+    const clients = asyncapi.extensions().get('x-remoteServers')
+    if(clients && clients[server.id()]){
+      return <File name={server.id() + '.ts'}>
+        <Text indent={0} newLines={1}>{`export async function clientAuth() {`}</Text>
+        <Text indent={1} newLines={1}>{'// You can add your authentication logic here.'}</Text>
+        <Text indent={1} newLines={1}>{`throw new Error("Function 'auth/${server.id()}' is not yet implemented.")`}</Text>
+        <Text indent={0} newLines={1}>{'}'}</Text>
+      </File>
+    }
+    return <File name={server.id() + '.ts'}>
+    <Text indent={0} newLines={1}>{`export async function serverAuth() {`}</Text>
+    <Text indent={1} newLines={1}>{'// You can add your authentication logic here.'}</Text>
+    <Text indent={1} newLines={1}>{`throw new Error("Function 'auth/${server.id()}' is not yet implemented.")`}</Text>
+    <Text indent={0} newLines={1}>{'}'}</Text>
+  </File>
+  
+  })
 }
--- template/functions/functions.ts.js
+++ template/functions/functions.ts.js
@@ -5,7 +5,8 @@ export default async function ({ asyncapi }) {
   return asyncapi.operations().filterByReceive().map(operation => (<File name={operation.id() + '.ts'}>
   <Text indent={0} newLines={2}>{'import { GleeFunction } from "@asyncapi/glee"'}</Text>
   <Text indent={0} newLines={1}>{`const ${operation.id()}: GleeFunction = async ({ payload }) => {`}</Text>
-  <Text indent={1} newLines={1}>{'// Your business logic goes here...'}</Text>
+  <Text indent={1} newLines={1}>{'// You can add your implementation logic here.'}</Text>
+  <Text indent={1} newLines={1}>{`throw new Error("Function '${operation.id()}' is not yet implemented.")`}</Text>
   <Text indent={0} newLines={1}>{'}'}</Text>
   <Text indent={0} newLines={1}>{`export default ${operation.id()}`}</Text>
   </File>))

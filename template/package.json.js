38024af01a694d1440c7a4de07a5118eb1b74930
Khuda Dad Nomani <khudadad414@gmail.com>
2024-01-17 12:16:21 +0000

    use latest glee version.

--- template/package.json.js
+++ template/package.json.js
@@ -1,13 +1,15 @@
-import { File } from '@asyncapi/generator-react-sdk'
+import { File } from "@asyncapi/generator-react-sdk"
 
-
-export default function ({asyncapi}) {
+export default async function ({ asyncapi }) {
   const description = asyncapi.info().description()
-  return <File name={"package.json"}>{
-    `{
+  const response = await fetch("https://registry.npmjs.org/@asyncapi/glee")
+  const data = await response.json()
+  const latestGleeVersion = data["dist-tags"].latest
+  return (
+    <File name={"package.json"}>{`{
   "name": "${asyncapi.info().title().toLowerCase().replace(/ /g, "-")}",
   "version": "0.1.0",
-  "description": "${description ? asyncapi.info().description().split('\n')[0]: ""}",
+  "description": "${description ? asyncapi.info().description().split("\n")[0] : ""}",
   "type": "module",
   "engines": {
     "node": ">=14.15.1"
@@ -20,11 +22,11 @@ export default function ({asyncapi}) {
     "glee"
   ],
   "dependencies": {
-    "@asyncapi/glee": "^0.32.18"
+    "@asyncapi/glee": "^${latestGleeVersion}"
   },
   "devDependencies": {
     "@types/node": "^20.5.9"
   }
-}`
-  }</File>
+}`}</File>
+  )
 }

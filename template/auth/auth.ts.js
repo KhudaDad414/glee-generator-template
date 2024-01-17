5e2bea0f865e74547d14ae07b6644c82d1bb0afb
Khuda Dad Nomani <khudadad414@gmail.com>
2024-01-17 12:18:38 +0000

    fix client server mismatch

--- template/auth/auth.ts.js
+++ template/auth/auth.ts.js
@@ -7,14 +7,14 @@ export default function ({ asyncapi }) {
     const clients = asyncapi.extensions().get('x-remoteServers')
     if(clients && clients[server.id()]){
       return <File name={server.id() + '.ts'}>
-        <Text indent={0} newLines={1}>{`export async function clientAuth() {`}</Text>
+        <Text indent={0} newLines={1}>{`export async function serverAuth() {`}</Text>
         <Text indent={1} newLines={1}>{'// You can add your authentication logic here.'}</Text>
         <Text indent={1} newLines={1}>{`throw new Error("Function 'auth/${server.id()}' is not yet implemented.")`}</Text>
         <Text indent={0} newLines={1}>{'}'}</Text>
       </File>
     }
     return <File name={server.id() + '.ts'}>
-    <Text indent={0} newLines={1}>{`export async function serverAuth() {`}</Text>
+    <Text indent={0} newLines={1}>{`export async function clientAuth() {`}</Text>
     <Text indent={1} newLines={1}>{'// You can add your authentication logic here.'}</Text>
     <Text indent={1} newLines={1}>{`throw new Error("Function 'auth/${server.id()}' is not yet implemented.")`}</Text>
     <Text indent={0} newLines={1}>{'}'}</Text>
